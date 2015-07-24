a3df5db3ed8585a866505fe43d9025f41003ac4e
Randall Rushing <randall.rushing@gmail.com>
2015-07-24 00:48:38 -0700

    Set number of tile columns [1/2]
    3, 4, or 5 columns. We don't want to get excessive here
    
    We keep the Quick Settings panel clean by scaling some
    dimens using a factor determined by the column count
    
    In QSPanel, we scale:
    -- mCellHeight
    -- mCellWidth
    -- and add mColumns as a member
    
    in QSTileView, we scale:
    -- mIconSizePx
    -- mTilePaddingBelowIconPx
    -- R.dimen.qs_tile_text_size (dynamically generated)
    -- and add mSizeScale as a member
    
    Change-Id: I6fc8652ce3fe41521958de04a86e59a15f9f1445
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/du_attrs.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -411,6 +411,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_IME_ARROWS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_NUM_TILE_COLUMNS),
+                     false, this, UserHandle.USER_ALL);
+            update();
         }
 
         @Override
@@ -469,6 +473,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             if (mIconPolicy != null) {
                 mIconPolicy.setCurrentUserSetup(mUserSetup);
+            }
+
+            if (mQSPanel != null) {
+                mQSPanel.updateNumColumns();
             }
         }
     };
