fb9e6830e7a14ba4d32db2b4f26ceeacc608afa1
htchoi1995 <htchoi1995@gmail.com>
2016-04-03 09:24:26 -0400

    [1/2] SystemUI: Network speed indicator
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/colors.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	packages/SystemUI/res/values/colors.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3898,6 +3898,12 @@ public final class Settings {
         public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
 
 
+
+        /* Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
